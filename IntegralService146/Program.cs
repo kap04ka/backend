77e871c6dd18e9d6a1bf2abc5e67504627ac779e
kap04ka <dani32oo@mail.ru>
2023-11-21 20:26:03 +0300

    ?)

--- IntegralService146/Program.cs
+++ IntegralService146/Program.cs
@@ -11,18 +11,6 @@ namespace IntegralService146
 
             var builder = WebApplication.CreateBuilder(args);
 
-            /*builder.Services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    policy =>
-                    {
-                        policy.WithOrigins("http://localhost:5020/swagger/",
-                                            "http://localhost:3000",
-                                            "http://localhost:3000/integral",
-                                            "http://localhost:3000/static/js/bundle.js:304524:14");
-                    });
-            });*/
-
             builder.Services.AddCors();
 
             // Add services to the container.
